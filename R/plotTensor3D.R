36ccb39332faa05bf4ab003ba07723829c7e6d99
kokitsuyuzaki <k.t.the-answer@hotmail.co.jp>
2021-09-02 17:07:01 +0900

    v-.1.1.5

--- R/plotTensor3D.R
+++ R/plotTensor3D.R
@@ -1,13 +1,12 @@
 plotTensor3D <- function(X = NULL, thr=2){
     if (is.array(X)){
-        X <- as.array(X)
+        X <- as.tensor(X)
     }
     coordinate <- expand.grid(1:dim(X)[1], 1:dim(X)[2], 1:dim(X)[3])
     colnames(coordinate) <- c("A1", "A2", "A3")
     allpoints <- as.vector(cs_unfold(X, m = 3)@data)
-    usingpoints <- which(abs((allpoints - mean(allpoints))/sd(allpoints)) >
+    usingpoints <- which((allpoints - mean(allpoints)/sd(allpoints)) >
         thr)
-    value <- smoothPalette(-allpoints, pal = "RdBu")
     if (length(usingpoints) != 0) {
         scatter3D(coordinate[usingpoints, 1], coordinate[usingpoints, 
             2], coordinate[usingpoints, 3], bty = "g", col = "blue", 
