c1d70738c08eaba4ed1cbad063dbf2d641e22840
koki <k.t.the-answer@hotmail.co.jp>
2024-05-13 21:35:07 +0900

    v1.3.0

--- R/kFoldMaskTensor.R
+++ R/kFoldMaskTensor.R
@@ -1,4 +1,4 @@
-kFoldMaskTensor <- function(X, k=3, seeds=123){
+kFoldMaskTensor <- function(X, k=3, seeds=123, sym=FALSE){
 	# Setting
 	set.seed(seeds)
 	# Input Type
@@ -6,23 +6,47 @@ kFoldMaskTensor <- function(X, k=3, seeds=123){
 	if(checkMatrix){
 		X <- as.tensor(X)
 	}
-	# Non-zero
-	position_nz <- which(X@data != 0)
-	# NA
-	position_na <- which(is.na(X@data))
-	# Shuffle
-	position_nz <- sample(position_nz, length(position_nz))
-	index <- suppressWarnings(split(position_nz, seq(k)))
-	# Output
-	out <- lapply(seq(k), function(x){
-		X@data[] <- 1
-		X@data[index[[x]]] <- 0
-		X@data[position_na] <- 0
-		if(checkMatrix){
-			X <- X@data
+	# Symmetric-Mode
+	if(sym){
+		if(!checkMatrix || !isSymmetric(X@data)){
+			stop("sym option is available only when matrix is specified")
 		}
-		X
-	})
+		# Non-zero in Upper Triangle Parts
+		position_nz_ut <- which(X@data[which(upper.tri(X@data))] != 0)
+		# NA
+		position_na_ut <- which(is.na(X@data[which(upper.tri(X@data))]))
+		# Shuffle
+		position_nz_ut <- sample(position_nz_ut, length(position_nz_ut))
+		index <- suppressWarnings(split(position_nz_ut, seq(k)))
+		# Output
+		out <- lapply(seq(k), function(x){
+			X@data[] <- 1
+			X@data[upper.tri(X@data)][index[[x]]] <- 0
+			X@data[upper.tri(X@data)][position_na_ut] <- 0
+			tX <- t(X@data)
+			X@data[lower.tri(X@data)] <- tX[lower.tri(tX)]
+			X@data
+		})
+	# Asymmetric-Mode
+	}else{
+		# Non-zero
+		position_nz <- which(X@data != 0)
+		# NA
+		position_na <- which(is.na(X@data))
+		# Shuffle
+		position_nz <- sample(position_nz, length(position_nz))
+		index <- suppressWarnings(split(position_nz, seq(k)))
+		# Output
+		out <- lapply(seq(k), function(x){
+			X@data[] <- 1
+			X@data[index[[x]]] <- 0
+			X@data[position_na] <- 0
+			if(checkMatrix){
+				X <- X@data
+			}
+			X
+		})
+	}
 	set.seed(NULL)
 	out
 }
